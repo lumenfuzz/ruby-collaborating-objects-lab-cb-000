49b5b21f843c031577f85c66ca14b287b426f121
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-25 17:07:30 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,11 +3,15 @@ class MP3Importer
 
   def initialize(file_path)
     @path = file_path
+    @files = []
   end
 
   def files
     full_path = @path + "/*.mp3"
-    @files = Dir[full_path]
+    full_file = Dir[full_path]
+    full_file.each do |file|
+      @files << file.split(@path)[0]
+    end
   end
 
   def import
